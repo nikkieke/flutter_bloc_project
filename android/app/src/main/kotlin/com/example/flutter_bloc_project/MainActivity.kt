6e80786c35db990de682779bf2235c8299ef6592
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-09-28 02:34:53 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_bloc_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bloc_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'features/crud/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+
