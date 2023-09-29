18c6b33354cb37baccbef9977ad0d56351d8f8fd
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-09-29 07:32:19 +0100

    example counter app impl

---
+++ examples/flutter_counter/lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_counter/view/counter_page.dart';
+
+class CounterApp extends MaterialApp{
+  CounterApp({super.key}): super(home: CounterPage());
+}
---
+++ examples/flutter_counter/lib/counter_observer.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+
+class CounterObserver extends BlocObserver{
+  const CounterObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change){
+    super.onChange(bloc, change);
+
+    //ignore: avoid_print
+    print("${bloc.runtimeType} $change");
+  }
+}
